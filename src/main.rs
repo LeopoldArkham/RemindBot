b67edcd3a9bdef1cd94f60c9559a6c12913d1146
LeopoldArkham <leopold.arkham@gmail.com>
2017-03-15 18:43:09 +0100

    Fleshing out the comment indentifier

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,22 @@ use hyper::header::{Authorization, UserAgent};
 use hyper::net::HttpsConnector;
 use hyper_native_tls::NativeTlsClient;
 
-// PAT : 303328dd10a0e3b8f13ff3606f9af7e581eea283
 
 // Webhooks for notifications?
 // A second mention within one thread overrides the previous one
+
+// TODO: Use Modified-Since headers to limit API polling exhaustion
+// TODO: Identify reply address for Issue-threads
+// TODO: Generalize issue workflow to PR's
+
 
 fn main() {
     let ssl = NativeTlsClient::new().unwrap();
     let connector = HttpsConnector::new(ssl);
     let client = Client::with_connector(connector);
 
-    let auth = Authorization("token 303328dd10a0e3b8f13ff3606f9af7e581eea283".to_string());
+    let tok = include_str!("../token.txt");
+    let auth = Authorization(format!("token {}", tok));
     let agent = UserAgent("remindbot".to_string());
 
     let mut resp = client.get("https://api.github.com/notifications")
@@ -32,21 +37,36 @@ fn main() {
     let mut buf = String::new();
 
     let _ = resp.read_to_string(&mut buf);
-    println!("{}", buf);
+    // println!("{}", buf);
     let parsed = json::parse(&buf).unwrap();
 
     for n in parsed.members() {
         if n["reason"] == "mention" {
             let comment_url = n["subject"]["latest_comment_url"].as_str().unwrap();
-            let mut resp =
-                client.get(comment_url).header(auth.clone()).header(agent.clone()).send().unwrap();
-            buf.drain(..);
-            let _ = resp.read_to_string(&mut buf);
-            let parsed_comment = json::parse(&buf).unwrap();
-            println!("url: {}\nBody: {}\nAuthor: {}\n",
-                     parsed_comment["url"],
-                     parsed_comment["body"],
-                     parsed_comment["user"]["login"]);
+            println!("{}", comment_url);
+            // let mut resp =
+            //     client.get(comment_url).header(auth.clone()).header(agent.clone()).send().unwrap();
+            // buf.drain(..);
+            // let _ = resp.read_to_string(&mut buf);
+            // let parsed_comment = json::parse(&buf).unwrap();
+
+            // let issue_url = if parsed_comment["issue_url"] == json::JsonValue::Null {
+            //     parsed_comment["url"].as_str().unwrap()
+            // } else {
+            //     parsed_comment["issue_url"].as_str().unwrap()
+            // };
+
+            // let reply_url = format!("{}/comments", issue_url);
+
+            // // println!("reply_url: {}\nBody: {}\nAuthor: {}\n",
+            // //          reply_url,
+            // //          parsed_comment["body"],
+            // //          parsed_comment["user"]["login"]);
+
+            // let body = format!("{{ \"body\": \"@{} I serve at your will, O Ineffable One.\"}}", parsed_comment["user"]["login"]);
+            // println!("{}", body);
+            // let mut resp = client.post(&reply_url).header(auth.clone()).header(agent.clone()).body(&body).send().unwrap();
+            // // println!("{}", resp.headers);
         }
     }
 }
