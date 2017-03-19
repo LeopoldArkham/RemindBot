a22099ba61b782e199cef6aac7ccfe8c7c2cc508
LeopoldArkham <leopold.arkham@gmail.com>
2017-03-19 15:29:12 +0100

    Comment cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,49 @@ extern crate json;
 
 use std::io::Read;
 
+// Hyper
 use hyper::Client;
 use hyper::header::{Authorization, UserAgent};
 use hyper::net::HttpsConnector;
+
+// hyper_native_tls
 use hyper_native_tls::NativeTlsClient;
 
 
-// Webhooks for notifications?
-// A second mention within one thread overrides the previous one
+// A second mention within one thread overrides the previous one.
+// Solution: Poll fast then mark thread as read.
 
 // TODO: Use Modified-Since headers to limit API polling exhaustion
 // TODO: Identify reply address for Issue-threads
 // TODO: Generalize issue workflow to PR's
+// TODO: Auto API polling every 2 seconds
 
+// struct Mention {
+
+// }
 
 fn main() {
+    // Setting up TLS
     let ssl = NativeTlsClient::new().unwrap();
     let connector = HttpsConnector::new(ssl);
     let client = Client::with_connector(connector);
 
+    // Setting up GitHub authentication
     let tok = include_str!("../token.txt");
     let auth = Authorization(format!("token {}", tok));
     let agent = UserAgent("remindbot".to_string());
 
+    // Send request and parse response containing notifications
     let mut resp = client.get("https://api.github.com/notifications")
         .header(auth.clone())
         .header(agent.clone())
         .send()
         .unwrap();
-
-    // println!("{}", resp.headers);
     let mut buf = String::new();
-
     let _ = resp.read_to_string(&mut buf);
-    // println!("{}", buf);
-    let parsed = json::parse(&buf).unwrap();
+    let parsed = json::parse(&buf).unwrap(); // <---------- Product
 
+    // Serialize mentions
     for n in parsed.members() {
         if n["reason"] == "mention" {
             let comment_url = n["subject"]["latest_comment_url"].as_str().unwrap();
